5d40723bce67a4adfc2ba6f1231e8964a4da795d
PatrikTorn <patrik.torn@fluentprogress.fi>
2019-01-31 12:10:19 +0200

    OK

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,7 +3,8 @@ import appReducer from './appReducer';
 import io from 'socket.io-client';
 import {SOCKET_ENDPOINT} from '../tools/actionTools'
 import configReducer from './configReducer';
-const socketReducer = io(SOCKET_ENDPOINT);
+// const socketReducer = io(SOCKET_ENDPOINT);
+const socketReducer = io('https://indecs.fi', {path:'/viikkis/socket', port:2021})
 
 // Combine all the reducers
 const rootReducer = combineReducers({
